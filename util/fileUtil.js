b6f19779dde901ccdec6584c12f85f645036d289
Andreas Partsch <Andreas.Partsch@sybit.de>
2020-12-06 21:39:33 +0100

    Extract getLinesOfFile

--- util/fileUtil.js
+++ util/fileUtil.js
@@ -3,5 +3,8 @@ const fs = require('fs')
 module.exports = {
   getInputOfCurrentDay (dirname) {
     return fs.promises.readFile(dirname + '/input.txt')
+  },
+  getLinesOfFile (file) {
+    return file.toString().split('\r\n')
   }
 }
