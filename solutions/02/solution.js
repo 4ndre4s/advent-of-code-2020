4f56c662aa13e34f588727db05a163d5ec257aed
Andreas Partsch <Andreas.Partsch@sybit.de>
2020-12-06 22:57:41 +0100

    Solve part 2 of day 2

--- solutions/02/solution.js
+++ solutions/02/solution.js
@@ -6,16 +6,35 @@ module.exports = async function () {
   const lines = fileUtil.getFilledLinesOfFile(file)
 
   const part1 = solvePartOne()
+  const part2 = solvePartTwo()
 
-  return new SolutionOfDay(2, part1)
+  return new SolutionOfDay(2, part1, part2)
 
-  function solvePartOne() {
+  function solvePartOne () {
     return lines.filter(line => {
-      const [intervals, letterSection, password]  = line.split(' ')
-      const [minOccurrences, maxOccurrences] = intervals.split('-')
-      const [letter] = letterSection
+      const { password, minOccurrences, maxOccurrences, letter } = getPasswordData(line)
       const actualOccurrences = password.split(letter).length - 1
+
       return actualOccurrences >= minOccurrences && actualOccurrences <= maxOccurrences
     }).length
+  }
+
+  function solvePartTwo () {
+    return lines.filter(line => {
+      const { password, minOccurrences, maxOccurrences, letter } = getPasswordData(line)
+      const firstIndex = minOccurrences - 1
+      const secondIndex = maxOccurrences - 1
+
+      return (password[firstIndex] === letter && password[secondIndex] !== letter)
+        || (password[firstIndex] !== letter && password[secondIndex] === letter)
+    }).length
+  }
+
+  function getPasswordData (line) {
+    const [intervals, letterSection, password] = line.split(' ')
+    const [minOccurrences, maxOccurrences] = intervals.split('-')
+    const [letter] = letterSection
+
+    return { password, minOccurrences, maxOccurrences, letter }
   }
 }
